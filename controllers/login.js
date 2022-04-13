cbce89f5bea6daa154804f5d2a517034461507b4
Tushar Grover <grow393@gmail.com>
2022-04-13 18:12:34 +0530

    jwt token and validation added

--- controllers/login.js
+++ controllers/login.js
@@ -1,44 +1,68 @@
 var mysql=require('mysql');
 var bcrypt=require('bcryptjs')
 require('dotenv').config()
+var jwt =require('jsonwebtoken');
 
 var connection=require('../dbConnection')
 
 exports.login=async(req,res)=>{
+  try {
     const {password,email}=req.body
-    const sqlSearch = "select * from `test_organization` where `Email`=? "
- const search_query = mysql.format(sqlSearch,[email])
- await connection.query (search_query, async (err, result) => {
-	if (err) throw (err)
-	console.log("------> Search Results")
-	console.log(result)
-	if (result.length != 0) {
-        const Password=result[0].Password
-      if(bcrypt.compareSync(password, Password)){
-
-        res.status(200).json({
-            Mesage:'Authorized User'
-        })
-
-      }
-      else{
-
-        res.status(400).json({
-            Mesage:'Invalid Password',
-            Name:result[0].Name,
-            Email:result[0].Email
-
-        })
-      }
-//	 connection.release()
+    if(email && password){
+      const sqlSearch = "select * from `test_organization` where `Email`=? "
+      const search_query = mysql.format(sqlSearch,[email])
+      await connection.query (search_query, async (err, result) => {
+       if (err) throw (err)
+       console.log("------> Search Results")
+       console.log(result)
+       if (result.length != 0) {
+             const Password=result[0].Password;
+             const mobileNo=result[0].Mobile_No;
+           if(bcrypt.compareSync(password, Password)){
+             
+          const token=   jwt.sign({
+               Email:email,
+               Mobile_No:mobileNo 
+             },process.env.secretKey,{expiresIn:'1d'})
+          console.log(token)
+             res.status(200).json({
+                 Mesage:'Authorized User',
+                 Name:result[0].Name,
+                 Email:result[0].Email
+             })
+     
+           }
+           else{
+     
+             res.status(400).json({
+                 Mesage:'Invalid Password, Try correct password'
+             })
+           }
+     
+       }
+       else{
+         res.status(400).json({
+                 Message:'Try signup first'
+             })
+       }
+     })
+    }
+    else{
+      return  res.status(400).json({
+        Message:'All fields are required'
+    })
+    }
+  
     
-	// res.sendStatus(409) 
-	}
-	else{
-		res.status(400).json({
-            Message:'Try signup first'
-        })
-	}
-})
+  } catch (error) {
+    console.log(error)
+  return  res.status(400).json({
+      Message:'Something went wrong'
+  })
+  
+
+    
+  }
+ 
 
 }
--- controllers/signup.js
+++ controllers/signup.js
@@ -7,38 +7,56 @@ var connection=require('../dbConnection')
 
 
 exports.signup=async(req,res)=>{
-    const {name,password,email,orgID,type,mobileNo}=req.body
-    const sqlSearch = "select * from `test_organization` where `Email`=? OR `OrgID`=? OR `Mobile NO.`=? "
- const search_query = mysql.format(sqlSearch,[email,orgID,mobileNo])
- await connection.query (search_query, async (err, result) => {
-	if (err) throw (err)
-	console.log("------> Search Results")
-	console.log(result)
-	if (result.length != 0) {
-//	 connection.release()
-	 console.log("------> User already exists")
-     res.status(400).json({
-         Mesage:'Try Different OrgId or mobile number or Email'
-     })
-	// res.sendStatus(409) 
-	}
-	else{
-		var hashPassword=  bcrypt.hashSync(password,10);
-		var insertQuery='insert into `test_organization` (`Name`,`OrgID`,`Email`,`Mobile NO.`,`Password`,`Type`) VALUES (?,?,?,?,?,?)'
-		var query=mysql.format(insertQuery,[name,orgID,email,mobileNo,
-		hashPassword,type]);
-		await connection.query (query, (err, result)=> {
-			//connection.release()
-			if (err) throw (err)
-			console.log ("--------> Created new User")
-			console.log(result.insertId)
-            console.log(result)
-			res.status(201).json({
-                Message:'Data Saved Successfully'
-
-            })
+	try {
+		const {name,password,email,orgID,type,mobileNo}=req.body
+		if(name && password && email && orgID && type && mobileNo ){
+			const sqlSearch = "select * from `test_organization` where `Email`=? OR `OrgID`=? OR `Mobile_No`=? "
+			const search_query = mysql.format(sqlSearch,[email,orgID,mobileNo])
+			await connection.query (search_query, async (err, result) => {
+			   if (err) throw (err)
+			   console.log("------> Search Results")
+			   console.log(result)
+			   if (result.length != 0) {
+		   //	 connection.release()
+				console.log("------> User already exists")
+				res.status(400).json({
+					Mesage:'Try Different OrgId or mobile number or Email'
+				})
+			   // res.sendStatus(409) 
+			   }
+			   else{
+				   var hashPassword=  bcrypt.hashSync(password,10);
+				   var insertQuery='insert into `test_organization` (`Name`,`OrgID`,`Email`,`Mobile_No`,`Password`,`Type`) VALUES (?,?,?,?,?,?)'
+				   var query=mysql.format(insertQuery,[name,orgID,email,mobileNo,
+				   hashPassword,type]);
+				   await connection.query (query, (err, result)=> {
+					   //connection.release()
+					   if (err) throw (err)
+					   console.log ("--------> Created new User")
+					   console.log(result.insertId)
+					   console.log(result)
+					   res.status(201).json({
+						   Message:'Data Saved Successfully'
+		   
+					   })
+					  })
+			   }
 		   })
+		}
+		else{
+			return  res.status(400).json({
+			  Message:'All fields are required'
+		  })
+		  }
+	
+		
+	} catch (error) {
+		console.log(error)
+		return  res.status(400).json({
+			Message:'Something went wrong'
+		})
+		
 	}
-})
+  
 
 }
